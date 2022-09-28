3cecf04b967ad6b4ec7dcd9379dfc15d38093a88
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-09-28 17:43:17 -0400

    Initial Commit

---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Abdulaziz Albahar on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
